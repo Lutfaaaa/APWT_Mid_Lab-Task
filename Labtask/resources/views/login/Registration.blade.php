843b199cd2ef2c8d877bf9eefa71c109ed9bcd45
Lutfa Alam@DESKTOP-OICST80 <lutfaalam21@gmail.com>
2021-06-23 17:59:19 +0600

    M3F1

---
+++ Labtask/resources/views/login/Registration.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Registration Page</title>
+</head>
+
+<body>
+    <form method="post">
+        <table>
+            <tr>
+                <td>Full Name</td>
+                <td><input type="text" name="fname" placeholder="Enter your Full Name"></td>
+            </tr>
+            <tr>
+                <td>User Name</td>
+                <td><input type="text" name="uname" placeholder="Enter your username"></td>
+            </tr>
+            <tr>
+                <td>Email</td>
+                <td><input type="email" name="email" placeholder="Enter your Email"></td>
+            </tr>
+            <tr>
+                <tr>
+                    <td>Password</td>
+                    <td><input type="password" name="password" placeholder="Enter your Password"></td>
+                </tr>
+    
+                <tr>
+                    <td>Confirm Password</td>
+                    <td><input type="password" name="cpassword" placeholder="Enter your Confirm Password"></td>
+                </tr>
+                <tr>
+                    <td>Address</td>
+                    <td><input type="text" name="address" placeholder="Enter your Address"></td>
+                </tr>
+                <tr>
+                    <td>Company Name</td>
+                    <td><input type="text" name="cname" placeholder="Enter your Company name"></td>
+                </tr>
+                <tr>
+                    <td>Phone Number</td>
+                    <td><input type="number" name="pnumber" placeholder="Enter your Phone number"></td>
+                </tr>
+                <tr>
+                    <td>City</td>
+                    <td><input type="text" name="city" placeholder="Enter your city"></td>
+                </tr>
+                <tr>
+                    <td>Country</td>
+                    <td><input type="text" name="country" placeholder="Enter your country"></td>
+                </tr>
+                <td></td>
+                <td><input type="submit" name="Submit" value="Sign up"></td>
+            </tr>
+        </table>
+    </form>
+    
+</body>
+
+</html>
